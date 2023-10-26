deb0459765aeaf78ca3b65baac95f8b2dbc8921e
skkra0 <73250770+skkra0@users.noreply.github.com>
2023-10-26 14:30:38 -0400

    Update main.h

--- main.h
+++ main.h
@@ -1,9 +1,12 @@
 #if !defined(MAIN_H)
 #define MAIN_H 1
+#include <array>
 
 const int END_VALUE = 0x2;
 const int JUMP_VALUE = 0x1E;
 const int CALL_VALUE = 0x4;
 const int IF_VALUE = 0x1F;
+
+const pair<string, int> commands[1058];
 
 #endif
